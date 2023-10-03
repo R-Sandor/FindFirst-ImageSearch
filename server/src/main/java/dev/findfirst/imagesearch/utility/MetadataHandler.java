3d6f46be1ea1be8cbb014a51e7df874d6ccbf572
Raphael J. Sandor <45048465+R-Sandor@users.noreply.github.com>
2023-10-03 16:43:28 -0400

    At point to handle predicting figures

--- server/src/main/java/dev/findfirst/imagesearch/utility/MetadataHandler.java
+++ server/src/main/java/dev/findfirst/imagesearch/utility/MetadataHandler.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -36,7 +37,8 @@ public class MetadataHandler {
         // List of regionless  captioned figures.
         var regionless = (List<Map>) rawTypes.get(("regionless-captions"));
 
-        var figures =
+        // Used map to handle key collision with merge function.
+        var figures = 
             Stream.of(formattedFigures, rawFigListMap, regionless)
                 .flatMap(listOfMap -> listOfMap.stream())
                 .filter(m -> m != null) // Skipping nulls
@@ -49,8 +51,6 @@ public class MetadataHandler {
                           return (!x.figName().equals("") && !x.type().equals("")) ? x : y;
                         }));
 
-        for (var f : figures.values()) System.out.println(f);
-
         this.saveMetaDataToDb(figures.values(), p);
       } catch (IOException e) {
         log.error(e.toString());
@@ -59,10 +59,49 @@ public class MetadataHandler {
   }
 
   private void saveMetaDataToDb(Collection<MetaData> figures, Path jsonPath) {
-    var fname = jsonPath.getFileName();
+    var fname = jsonPath.getFileName().toString();
     log.info("filename {}", fname);
+    final var prefix = fname.split("deepfigures*")[0];
+    final var dbIDPrefix = prefix + ".pdf-";
+    log.info("prefix {}", prefix);
+    // Example of what an imageId looks like from the database: P14-2074.pdf-Figure1.
+    // Split the file name before deep figure.
+    var figureMap = new HashMap<String, MetaData>();
+    figures.stream().forEach( 
+      metaData -> {
+        var imageId =  documentID(dbIDPrefix, metaData);
+        figureMap.put(imageId, metaData);
+    });
 
-    /// Split the file name before deep figure
+    // Predict the figures types.
+    var updatedMetaData = this.predictFigures(figureMap.values().stream().filter(md -> md.type().equals("Figure")));
+    updatedMetaData.forEach(newData -> figureMap.put(documentID(dbIDPrefix, newData) , newData));
 
+    log.info("imageIds {}", figureMap.values());
+
+  }
+
+  /**
+   * Calls the database on those documents that do not have a figure type. 
+   * The highest confidence of the document types (barchart, scatterplot, etc) is then 
+   * saved to the metadata then latter to the document for facid search 
+   * @param stream
+   * @return
+   */
+  private List<MetaData> predictFigures(Stream<MetaData> stream) {
+      return null;
+  }
+
+  /**
+   * Utility function to get the documentID
+   * @param prefix the prefix of the document; Obtained by JSON filename.
+   * @param metaData the actual MetaData to get figure, name.
+   * @return documentID
+   */
+  private String documentID(String prefix, MetaData metaData) {
+    return prefix +  metaData.type() + metaData.figName();
+  }
+
+  private void predictFigures(List<String> imageIds) {
   }
 }
