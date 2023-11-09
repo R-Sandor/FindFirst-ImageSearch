e57e1a16faf8a7fb4bce157f5b9fc40f6d530dd5
Raphael J. Sandor <45048465+R-Sandor@users.noreply.github.com>
2023-11-09 05:54:44 -0500

    uses correct compose project name

--- bckups/backupElastic.sh
+++ bckups/backupElastic.sh
@@ -1,3 +1,3 @@
 #! /bin/sh 
-docker run --rm --volumes-from es-docker-es01-1 -v $(pwd):/backup ubuntu tar cvf backup/esdata01-backup.tar /usr/share/elasticsearch/data
-docker run --rm --volumes-from es-docker-es02-1 -v $(pwd):/backup ubuntu tar cvf backup/esdata02-backup.tar /usr/share/elasticsearch/data
+docker run --rm --volumes-from findfirst-es01-1 -v $(pwd):/backup ubuntu tar cvf backup/esdata01-backup.tar /usr/share/elasticsearch/data
+docker run --rm --volumes-from findfirst-es02-1 -v $(pwd):/backup ubuntu tar cvf backup/esdata02-backup.tar /usr/share/elasticsearch/data
